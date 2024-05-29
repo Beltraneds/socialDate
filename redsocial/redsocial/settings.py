f4d5776f91ac4fc46574a4664daab83115d32648
Beltraneds <beltranjr.alexander@gmail.com>
2024-05-29 04:09:49 -0400

    "Creación incial del index y configuración inicial"

--- redsocial/redsocial/settings.py
+++ redsocial/redsocial/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'redsocial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'socialdate',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3307',
     }
 }
 
@@ -116,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Media files (Uploaded by users)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- redsocial/socialdate/models.py
+++ redsocial/socialdate/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
 # Create your models here.
+
+class Genero(models.Model):
+    nombre = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+
+class Interes(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class TipoUsuario(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido_paterno = models.CharField(max_length=50)
+    apellido_materno = models.CharField(max_length=50, blank=True, null=True)
+    correo = models.EmailField(max_length=80, unique=True)
+    contrasena = models.CharField(max_length=18)
+    foto_perfil = models.ImageField(upload_to='fotos_perfil/')
+    fecha_nacimiento = models.DateField()
+    tipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido_paterno}"
+
+class Chat(models.Model):
+    nombre_chat = models.CharField(max_length=30)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre_chat
+
+class Foto(models.Model):
+    imagen = models.ImageField(upload_to='fotos/')
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+class Like(models.Model):
+    cantidad = models.IntegerField()
+
+class Match(models.Model):
+    descripcion = models.CharField(max_length=80)
+    cantidad = models.IntegerField()
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+
+class UsuarioInteres(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    interes = models.ForeignKey(Interes, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('usuario', 'interes')
+
+class UsuarioLike(models.Model):
+    like = models.ForeignKey(Like, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('like', 'usuario')
+
+class UsuarioMatch(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('usuario', 'match')
